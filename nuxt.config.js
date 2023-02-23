61ee54fea64fd3c1395bec7ed6ef19c6de37e3d0
bwealthy72 <bwealthy72@gmail.com>
2023-02-23 21:40:05 +0900

    favicon 추가 및 head에서 meta 구글 서치콘솔 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,4 +5,17 @@ export default defineNuxtConfig({
     // ...
     "@pinia/nuxt",
   ],
+  app: {
+    head: {
+      charset: "utf-16",
+      viewport: "initial-scale=1",
+      title: "Vuelog's Blog",
+      meta: [
+        {
+          name: "google-site-verification",
+          content: "CyvFDg064KG5R0wkwhlZdsVdG5zElVLGvRiGMJIuV8Q",
+        },
+      ],
+    },
+  },
 });
