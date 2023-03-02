6f98deedc9540c14974ccf4d33ec206dba43b68f
bwealthy72 <bwealthy72@gmail.com>
2023-03-02 16:19:30 +0900

    image를 blob으로 만들었음

--- pages/post.vue
+++ pages/post.vue
@@ -9,20 +9,44 @@ const route = useRoute();
 const { setPage } = usePostStore();
 
 const request = async function () {
-  setPage(null);
-  const { data } = await useFetch("/api/page", {
-    method: "post",
-    body: { blockId: route.params.id },
-  });
-  setPage(data.value);
-  useSeoMeta({
-    title: data.value?.title,
-    ogTitle: data.value?.title,
-    description: data.value?.description,
-    ogDescription: data.value?.description,
-    ogImage: data.value?.cover,
-    twitterCard: "summary_large_image",
-  });
+  if (route.params.id) {
+    setPage(null);
+    const { data } = await useFetch("/api/page", {
+      method: "post",
+      body: { blockId: route.params.id },
+    });
+    setPage(data.value);
+
+    if (data.value?.cover) {
+      fetch(data.value?.cover)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(blob);
+          reader.onloadend = () => {
+            const imageBase64 = reader.result.split(",")[1];
+            const image = `data:image/jpeg;base64,${imageBase64}`;
+
+            useSeoMeta({
+              title: data.value?.title,
+              ogTitle: data.value?.title,
+              description: data.value?.description,
+              ogDescription: data.value?.description,
+              ogImage: image,
+              twitterCard: "summary_large_image",
+            });
+          };
+        });
+    } else {
+      useSeoMeta({
+        title: data.value?.title,
+        ogTitle: data.value?.title,
+        description: data.value?.description,
+        ogDescription: data.value?.description,
+        twitterCard: "summary_large_image",
+      });
+    }
+  }
 };
 
 watch(
