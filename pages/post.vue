8b052000c1c46a98fbb0c27b4da8a5fed3cf55ef
bwealthy72 <bwealthy72@gmail.com>
2023-03-05 22:51:08 +0900

    open graph를 위해 await으로 변경

--- pages/post.vue
+++ pages/post.vue
@@ -8,35 +8,41 @@ const route = useRoute();
 
 const { setPage } = usePostStore();
 
-const request = function () {
+const request = async function () {
   if (route.params.id && route.params.id.includes("-")) {
     setPage(null);
-    useFetch("/api/page", {
+    const { data } = await useFetch("/api/page", {
       method: "post",
       body: { blockId: route.params.id },
-    }).then(({ data }) => {
-      setPage(data.value);
+    });
 
-      let image = data.value?.cover;
-      if (image) {
-        // 바꾸지 않으면 일부 값이 사라짐
-        image = image.replaceAll("%2F", "/");
-      }
+    setPage(data.value);
 
-      // useHead로 써보자 값은 제대로 들어가는 모양인데 왜 west-2로 들어갈까
-      useSeoMeta({
-        title: data.value?.title,
-        ogTitle: data.value?.title,
-        description: data.value?.description,
-        ogDescription: data.value?.description,
-        ogImage: image,
-        twitterCard: "summary_large_image",
-      });
+    let image = data.value?.cover;
+    if (image) {
+      // 바꾸지 않으면 일부 값이 사라짐
+      image = image.replaceAll("%2F", "/");
+    }
+
+    // useHead로 써보자 값은 제대로 들어가는 모양인데 왜 west-2로 들어갈까
+    useSeoMeta({
+      title: data.value?.title,
+      ogTitle: data.value?.title,
+      description: data.value?.description,
+      ogDescription: data.value?.description,
+      ogImage: image,
+      twitterCard: "summary_large_image",
     });
   }
 };
 
-watch(route, request, { immediate: true });
+watch(
+  route,
+  async () => {
+    await request();
+  },
+  { immediate: true }
+);
 
 onMounted(() => {
   const { openWindow } = useWindowStore();
