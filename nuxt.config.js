df4d1b7df74f163ff4fa7cd3753114d84c52ff59
bwealthy72 <bwealthy72@gmail.com>
2023-03-15 11:14:51 +0900

    adsense 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default defineNuxtConfig({
       script: [
         {
           async: true,
-          src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8123307370591079",
+          src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2649674681017228",
         },
       ],
     },
