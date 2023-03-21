2d15c33e5f3e16e0e2f113fdc3ae308a716f8dfc
bwealthy72 <bwealthy72@gmail.com>
2023-03-21 13:33:48 +0900

    Coupang Partners 닫기버튼 추가

--- components/CoupangPartners.vue
+++ components/CoupangPartners.vue
@@ -1,53 +1,112 @@
 <template>
-  <div class="coupang-partners">
-    <!-- X 이미지 추가 -->
-    <iframe
-      src="https://ads-partners.coupang.com/widgets.html?id=644983&template=carousel&trackingCode=AF0397919&subId=&width=350&height=60"
-      width="350"
-      height="60"
-      frameborder="0"
-      scrolling="no"
-      referrerpolicy="unsafe-url"
-    ></iframe>
-    <p>
-      쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다.
-    </p>
+  <div class="coupang-partners" v-if="show">
+    <div class="close-btn" @click="show = false">
+      <img src="@/assets/images/close.svg" alt="coupang-close" />
+    </div>
+    <div class="content">
+      <iframe
+        src="https://ads-partners.coupang.com/widgets.html?id=644983&template=carousel&trackingCode=AF0397919&subId=&width=350&height=60"
+        width="300"
+        height="150"
+        frameborder="0"
+        scrolling="no"
+        referrerpolicy="unsafe-url"
+      ></iframe>
+      <p>
+        쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다.
+      </p>
+      <p>(마우스 커서를 가져다 대면 닫기버튼이 나타납니다.)</p>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const show = ref(false);
+
+watch(show, () => {
+  localStorage.setItem("vuelog-coupang-show", show.value);
+});
+onMounted(() => {
+  const s = localStorage.getItem("vuelog-coupang-show");
+  if (s) {
+    show.value = s == "true";
+  } else {
+    show.value = true;
+  }
+});
+</script>
 
 <style lang="scss" scoped>
 @import "@/assets/scss/base/variable.scss";
+
+@keyframes slide {
+  0% {
+    transform: translateX(100%);
+  }
+
+  100% {
+    transform: translateX(0);
+  }
+}
+
 .coupang-partners {
   position: absolute;
-  top: 0;
-  right: 0;
-  padding: 1rem 2rem;
+  top: 1rem;
+  right: 1rem;
   box-sizing: border-box;
-  background-color: rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(4px);
-  border-radius: 1rem;
   user-select: none;
+  animation: slide 0.7s;
 
-  p {
-    color: white;
-    margin-top: 0.5rem;
-    text-align: center;
+  &:hover {
+    .close-btn {
+      opacity: 1;
+    }
+  }
+
+  .close-btn {
+    opacity: 0;
+    transition: opacity 0.3s;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: translate(-30%, -30%);
+    margin-left: auto;
+    z-index: 1;
+    width: 2.5rem;
+    height: 2.5rem;
+    padding: 0.6rem;
+    box-sizing: border-box;
+    border-radius: 50%;
+    background-color: #222;
+    border: 1px solid #555;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .content {
+    background-color: #222;
+    backdrop-filter: blur(4px);
+    padding: 1rem 2rem;
+    border-radius: 1rem;
+    border: 1px solid #555;
+    iframe {
+      width: 100%;
+    }
+    p {
+      color: white;
+      margin-top: 0.5rem;
+      text-align: center;
+    }
   }
 }
 
 @media (max-width: $breakpoint-tablet) {
   .coupang-partners {
-    width: 100%;
-    height: 200px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    iframe {
-      width: 100%;
-      height: 100%;
-    }
+    max-width: calc(100% - 2rem);
+    min-width: calc(100% - 2rem);
   }
 }
 </style>
