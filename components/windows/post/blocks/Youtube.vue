6848daf79113c0d6e9e3e1f6a8a9465287879e60
bwealthy72 <bwealthy72@gmail.com>
2023-04-01 00:00:59 +0900

    Youtube 설정 추가

--- components/windows/post/blocks/Youtube.vue
+++ components/windows/post/blocks/Youtube.vue
@@ -5,18 +5,33 @@
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen
+    class="youtube"
   ></iframe>
 </template>
 
 <script setup>
 const { block } = defineProps(["block"]);
+console.log(block);
 const videoId = computed(() => {
   const url = block[block.type]?.external?.url;
+  console.log(url);
   if (url) {
     const u = new URL(url);
-    return u.searchParams.get("v");
+    let value = u.searchParams.get("v");
+    if (value) {
+      return value;
+    } else {
+      const str = url.split("/");
+      console.log(str);
+      return str[str.length - 1];
+    }
   }
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.youtube {
+  aspect-ratio: 1.6 / 1;
+  border-radius: 1rem;
+}
+</style>
