55a11f640bf33dd2dd6d9f558ea75763595be2bc
bwealthy72 <bwealthy72@gmail.com>
2023-05-05 14:35:37 +0900

    async 대신 then 을 사용

--- pages/post.vue
+++ pages/post.vue
@@ -9,30 +9,31 @@ const route = useRoute();
 const { setPage } = usePostStore();
 
 // 여기서 request 하는 이유는 SEO가 여기서 되기 때문이다.
-const request = async function () {
+const request = function () {
   const number = parseInt(route.params.id);
   if (Number.isInteger(number)) {
     setPage(null);
-    const { data } = await useFetch("/api/post/page", {
+    useFetch("/api/post/page", {
       method: "post",
       body: { number },
-    });
-    setPage(data.value);
+    }).then(({ data }) => {
+      setPage(data.value);
 
-    const page = data.value;
-    let image = page?.cover;
-    if (image) {
-      // 바꾸지 않으면 일부 값이 사라짐
-      image = image.replaceAll("%2F", "/");
-    }
+      const page = data.value;
+      let image = page?.cover;
+      if (image) {
+        // 바꾸지 않으면 일부 값이 사라짐
+        image = image.replaceAll("%2F", "/");
+      }
 
-    useSeoMeta({
-      title: page?.title,
-      ogTitle: page?.title,
-      description: page?.description,
-      ogDescription: page?.description,
-      ogImage: image,
-      twitterCard: "summary_large_image",
+      useSeoMeta({
+        title: page?.title,
+        ogTitle: page?.title,
+        description: page?.description,
+        ogDescription: page?.description,
+        ogImage: image,
+        twitterCard: "summary_large_image",
+      });
     });
   }
 };
